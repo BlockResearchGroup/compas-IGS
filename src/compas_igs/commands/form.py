5a7c8c862ea5a5cbb56f95d63ae610db5e9537d0
tomvanmele <tom.v.mele@gmail.com>
2024-11-14 18:00:55 +0100

    remove unused commands

--- src/compas_igs/commands/form.py
+++ src/compas_igs/commands/form.py
@@ -5,9 +5,7 @@ import rhinoscriptsyntax as rs  # type: ignore
 import compas_rhino.objects
 from compas_ags.diagrams import FormDiagram
 from compas_ags.diagrams import FormGraph
-from compas_igs.scene import RhinoFormObject
 from compas_rui.forms import FileForm
-from compas_rui.forms import NamedValuesForm
 
 
 def create_from_lines():
@@ -54,16 +52,3 @@ def create_from_obj(basedir):
 
     formdiagram = FormDiagram.from_graph(graph)
     return formdiagram
-
-
-def assign_forces(formobject: RhinoFormObject):
-    edges = list(formobject.diagram.edges_where(is_ind=True))
-    if not edges:
-        return rs.MessageBox(message="Please identify the independent edges first.")
-
-    names = [str(edge) for edge in edges]
-    values = [formobject.diagram.edge_attribute(edge, name="f") for edge in edges]
-    form = NamedValuesForm(names, values, title="Assign Forces")
-    if form.show():
-        for edge, (name, value) in zip(edges, form.attributes.items()):
-            formobject.diagram.edge_attribute(edge, name="f", value=value)
